0c954fff8e1721a5e1afc0270711a22b56fb468d
Omar Mahat <omarmahat702@gmail.com>
2023-06-22 21:38:42 +0300

    structured the navigation links for different pages

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const hamburger = document.querySelector(".hamburger")
 const navBar = document.querySelector(".nav-div-two")
 const navLink = document.querySelectorAll(".nav-link")
-const mainSection = document.querySelector(".main-section")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navBar.classList.toggle("active")
 })
-
-// mainSection.addEventListener("click", () => {
-//     hamburger.classList.remove("active")
-//     navBar.classList.remove("active")
-// })
 
 navLink.forEach(link => {
     link.addEventListener("click", () => {
